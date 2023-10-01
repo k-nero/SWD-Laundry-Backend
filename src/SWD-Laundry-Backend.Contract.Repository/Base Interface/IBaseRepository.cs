5d0afc3394d0b61a99140c0a6d1914d32dcc4c40
Nero <67089844+thanhplassma@users.noreply.github.com>
2023-10-01 11:57:08 +0700

    Update Repository

--- src/SWD-Laundry-Backend.Contract.Repository/Base Interface/IBaseRepository.cs
+++ src/SWD-Laundry-Backend.Contract.Repository/Base Interface/IBaseRepository.cs
@@ -1,11 +1,12 @@
 ﻿using System.Linq.Expressions;
+using Microsoft.EntityFrameworkCore.Query;
 
 namespace SWD_Laundry_Backend.Contract.Repository.Base_Interface;
 public interface IBaseRepository<T> where T : Entity.BaseEntity, new()
 {
-    T GetSingle(Expression<Func<T, bool>>? filter = null, params Expression<Func<T, object>>[]? includes);
-    IQueryable<T> Get(Expression<Func<T, bool>>? filter = null, params Expression<Func<T, object>>[]? includes);
-    int Update(Expression<Func<T, bool>> filter, T entity);
-    int Delete(Expression<Func<T, bool>> filter);
-    int Add(T entity);
+    Task<T?> GetSingleAsync(Expression<Func<T, bool>>? filter = null, params Expression<Func<T, object>>[]? includes);
+    Task<IQueryable<T>> GetAsync(Expression<Func<T, bool>>? filter = null, params Expression<Func<T, object>>[]? includes);
+    Task<int> UpdateAsync(Expression<Func<T, bool>> filter, Expression<Func<SetPropertyCalls<T>, SetPropertyCalls<T>>> update);
+    Task<int> DeleteAsync(Expression<Func<T, bool>> filter);
+    Task<T> AddAsync(T entity);
 }

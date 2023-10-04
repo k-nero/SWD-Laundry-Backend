3e5b59555f20e3e13f38c87363953334b1f4978c
Nero <67089844+thanhplassma@users.noreply.github.com>
2023-10-04 16:28:34 +0700

    Fix

--- src/SWD-Laundry-Backend.Repository/Repository/WalletRepository.cs
+++ src/SWD-Laundry-Backend.Repository/Repository/WalletRepository.cs
@@ -1,5 +1,6 @@
 ﻿using Invedia.DI.Attributes;
 using SWD_Laundry_Backend.Contract.Repository.Entity;
+using SWD_Laundry_Backend.Contract.Repository.Infrastructure;
 using SWD_Laundry_Backend.Contract.Repository.Interface;
 using SWD_Laundry_Backend.Repository.Infrastructure;
 
@@ -8,7 +9,7 @@ namespace SWD_Laundry_Backend.Repository.Repository;
 [ScopedDependency(ServiceType = typeof(IWalletRepository))]
 public class WalletRepository : Repository<Wallet>, IWalletRepository
 {
-    public WalletRepository(AppDbContext dbContext) : base(dbContext)
+    public WalletRepository(AppDbContext dbContext, ICacheLayer<Wallet> cachelayer) : base(dbContext, cachelayer)
     {
     }
 }

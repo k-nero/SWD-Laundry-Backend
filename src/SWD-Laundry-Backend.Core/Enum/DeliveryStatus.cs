32997dde13a7576d73988823437ccd8ed3135425
Nero <67089844+k-nero@users.noreply.github.com>
2023-10-15 22:45:50 +0700

    small update

--- src/SWD-Laundry-Backend.Core/Enum/DeliveryStatus.cs
+++ src/SWD-Laundry-Backend.Core/Enum/DeliveryStatus.cs
@@ -1,17 +1,11 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace SWD_Laundry_Backend.Core.Enum;
+﻿namespace SWD_Laundry_Backend.Core.Enum;
 public enum DeliveryStatus
 {
     Pending = 0,
-    Delivering_Laundry = 1,
-    Reached_Laundry = 2,
-    Delivering_Customer = 3,
-    Reached_Customer = 4,
+    DeliveringLaundry = 1,
+    ReachedLaundry = 2,
+    DeliveringCustomer = 3,
+    ReachedCustomer = 4,
     Delivered = 5,
 
 }

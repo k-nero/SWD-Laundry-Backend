da1123b1489226c943660a0a38a5c0c20edb002e
Nero <67089844+k-nero@users.noreply.github.com>
2023-10-15 23:04:35 +0700

    PaypalController

--- src/SWD-Laundry-Backend/Controllers/PaypalController.cs
+++ src/SWD-Laundry-Backend/Controllers/PaypalController.cs
@@ -1,15 +1,54 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using SWD_Laundry_Backend.Contract.Service.Interface;
+using SWD_Laundry_Backend.Core.Models;
+using SWD_Laundry_Backend.Core.Models.Common;
+using SWD_Laundry_Backend.Core.ValueObject;
+using static SWD_Laundry_Backend.Core.ValueObject.PaypalApiObjectModel.PaypalOrder;
 
 namespace SWD_Laundry_Backend.Controllers;
 
 [ApiController]
 public class PaypalController : ApiControllerBase
 {
-    [HttpPost]
-    [ProducesResponseType(StatusCodes.Status201Created)]
-    public async Task<IActionResult> CreatePayment()
+    private readonly IPaypalService _paypalService;
+
+    public PaypalController(IPaypalService paypalService)
     {
-        return Ok();
+        _paypalService = paypalService;
+    }
+
+    [HttpPost("/deposite-wallet")]
+    [ProducesResponseType(StatusCodes.Status201Created)]
+    [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+    [ProducesDefaultResponseType]
+    public async Task<IActionResult> CreatePayment(TransactionModel model)
+    {
+        try
+        {
+            var result = await _paypalService.CreatePaypalOrderAsync(model);
+            return Ok(new BaseResponseModel<PaypalOrderResponse>(StatusCodes.Status201Created, result));
+        }
+        catch (Exception e)
+        {
+            return BadRequest(new BaseResponseModel<string>(StatusCodes.Status500InternalServerError, e.Message));
+        }
+    }
+
+    [HttpGet("/capture-paypal-order/{orderId}")]
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+    [ProducesDefaultResponseType]
+    public async Task<IActionResult> CapturePaypalOrder(string orderId)
+    {
+        try
+        {
+            var result = await _paypalService.CapturePaypalOrderAsync(orderId);
+            return Ok(new BaseResponseModel<object?>(StatusCodes.Status200OK, result));
+        }
+        catch (Exception e)
+        {
+            return BadRequest(new BaseResponseModel<string>(StatusCodes.Status500InternalServerError, e.Message));
+        }
     }
 
 }

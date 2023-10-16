52ccdd09ac057a7bea725a1d5d1501c6877f880e
Nero <thanhnguyen2409@protonmail.com>
2023-10-17 01:02:45 +0700

    Update OrderHistoryQuery.cs

--- src/SWD-Laundry-Backend.Core/QueryObject/OrderHistoryQuery.cs
+++ src/SWD-Laundry-Backend.Core/QueryObject/OrderHistoryQuery.cs
@@ -4,7 +4,7 @@ namespace SWD_Laundry_Backend.Core.QueryObject;
 public record OrderHistoryQuery : BaseQuery
 {
     [FromRoute(Name = "order-id")]
-    public string? OrderId { get; set; }
+    public string? OrderId { get; init; }
     [FromQuery(Name = "customer-id")]
     public string? CustomerId { get; init; }
     [FromQuery(Name = "laundry-store-id")]
